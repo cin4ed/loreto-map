4098a2e1d835c6c12d7cd9b5a8f9625dbe841645
Kenneth Quintero <56099614+cin4ed@users.noreply.github.com>
2024-08-22 10:42:01 -0700

    feat: Add search and filter area

--- app/page.tsx
+++ app/page.tsx
@@ -1,110 +1,154 @@
 "use client";
 
-import mapboxgl from "mapbox-gl";
-import { useRef, useEffect, useState } from "react";
+import { useState } from "react";
 import { Separator } from "@/components/ui/separator";
+import { Badge } from "@/components/ui/badge";
+import Map from "@/components/map";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { MapIcon, SearchIcon, BuildingIcon, StoreIcon } from "lucide-react";
 
-mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAPBOX_TOKEN || "";
+const secondaryZoneTypes = [
+  {
+    id: 1,
+    key: "H",
+    name: "Habitacional",
+  },
+  {
+    id: 2,
+    key: "HP",
+    name: "Habitacional Plurifamiliar",
+  },
+  {
+    id: 3,
+    key: "TR",
+    name: "Turístico Residencial",
+  },
+  {
+    id: 4,
+    key: "CU",
+    name: "Comercio y Servicios",
+  },
+  {
+    id: 5,
+    key: "M",
+    name: "Mixto Comercial",
+  },
+  {
+    id: 6,
+    key: "E",
+    name: "Equipamiento",
+  },
+  {
+    id: 7,
+    key: "IN",
+    name: "Industrial",
+  },
+  {
+    id: 8,
+    key: "Z",
+    name: "Zona Verde",
+  },
+  {
+    id: 9,
+    key: "PC",
+    name: "Preservación Conservación",
+  },
+  {
+    id: 10,
+    key: "V",
+    name: "Vialidades",
+  },
+  {
+    id: 11,
+    key: "AT",
+    name: "Alojamiento Turístico",
+  },
+  {
+    id: 12,
+    key: "CO",
+    name: "Corredor Turístico",
+  },
+];
 
 export default function Home() {
-  const mapContainer = useRef<HTMLDivElement>();
-  const map = useRef<mapboxgl.Map | null>(null);
-  const [lng, setLng] = useState(-111.3426);
-  const [lat, setLat] = useState(26.0111);
-  const [zoom, setZoom] = useState(14.13);
-  const [selectedFeature, setSelectedFeature] = useState(null);
-  const [selectedFeatureProperties, setSelectedFeatureProperties] =
-    useState(null);
+  const [searchQuery, setSearchQuery] = useState("");
 
-  useEffect(() => {
-    // Initialize map only once
-    if (map.current) return;
-
-    map.current = new mapboxgl.Map({
-      container: mapContainer.current,
-      style: "mapbox://styles/kenneth-quintero/cm043gkrp00eo01pwcjwxfbmj",
-      center: [lng, lat],
-      zoom: zoom,
-    });
-
-    map.current.on("move", () => {
-      setLng(map.current.getCenter().lng.toFixed(4));
-      setLat(map.current.getCenter().lat.toFixed(4));
-      setZoom(map.current.getZoom().toFixed(2));
-    });
-
-    map.current.on("load", () => {
-      map.current.on("click", "ezone-uso-de-suelo", (e) => {
-        const properties = e.features[0].properties;
-        setSelectedFeatureProperties(properties);
-        console.log(properties);
-      });
-
-      // Add a source for the highlighted polygon
-      map.current.addSource("highlighted-polygon", {
-        type: "geojson",
-        data: {
-          type: "FeatureCollection",
-          features: [],
-        },
-      });
-
-      // Add a layer to display the highlighted polygon
-      map.current.addLayer({
-        id: "highlighted-polygon-layer",
-        type: "line",
-        source: "highlighted-polygon",
-        paint: {
-          "line-color": "#FF0000", // Outline color (e.g., red)
-          "line-width": 3, // Width of the outline
-        },
-      });
-
-      // Detect clicks on the polygon layer
-      map.current.on("click", "ezone-uso-de-suelo", function (e) {
-        var clickedPolygon = e.features[0];
-
-        // Update the source with the clicked polygon data
-        map.current.getSource("highlighted-polygon").setData({
-          type: "FeatureCollection",
-          features: [clickedPolygon],
-        });
-
-        // // Optionally, show the properties in a popup
-        // new mapboxgl.Popup()
-        //   .setLngLat(e.lngLat)
-        //   .setHTML(
-        //     `<strong>${clickedPolygon.properties.name}</strong><br>Type: ${clickedPolygon.properties.type}`
-        //   )
-        //   .addTo(map.current);
-      });
-
-      map.current.on("mouseenter", "ezone-uso-de-suelo", function () {
-        map.current.getCanvas().style.cursor = "pointer";
-      });
-
-      map.current.on("mouseleave", "ezone-uso-de-suelo", function () {
-        map.current.getCanvas().style.cursor = "";
-      });
-    });
-  }, []);
+  function handleSearch(e) {
+    e.preventDefault();
+  }
 
   return (
     <main className="min-h-screen grid grid-cols-6">
       <div className="p-4 col-span-2">
         <h1 className="text-2xl font-bold">eZon</h1>
         <Separator />
-        <h2 className="text-xl font-semibold mt-4">Propiedades</h2>
-        <p>Selecciona una zona para ver más información.</p>
-        <pre className="bg-gray-100 p-4 rounded-lg">
-          {JSON.stringify(selectedFeatureProperties, null, 2)}
-        </pre>
+        <div className="mt-4 space-y-5">
+          {/* Search Box */}
+          <div className="border rounded-lg p-2 bg-zinc-50">
+            <h3 className="font-bold">Buscar</h3>
+            <Separator className="mt-1" />
+            <form onSubmit={handleSearch} className="flex space-x-2 mt-2">
+              <Input
+                type="text"
+                placeholder="Calle Salvatierra 789, Colonia Centro, Loreto, BCS"
+                value={searchQuery}
+                onChange={(e) => setSearchQuery(e.target.value)}
+                className="flex-grow"
+              />
+              <Button type="submit">
+                <SearchIcon className="w-4 h-4 mr-2" />
+                Buscar
+              </Button>
+            </form>
+          </div>
+          {/* Filtros */}
+          <div className="border rounded-lg p-2 bg-zinc-50">
+            <h3 className="font-bold">Filtros</h3>
+            <Separator className="mt-1" />
+            <div className="mt-2 space-y-4">
+              <div>
+                <h4 className="text-xs font-bold">Tipo de Zona</h4>
+                <p className="text-xs text-muted-foreground mt-1">
+                  Selecciona uno o varios tipos de zona
+                </p>
+                <div className="grid grid-cols-2 gap-2 mt-2">
+                  {secondaryZoneTypes.map((type) => (
+                    <div
+                      key={type.id}
+                      className="text-xs border rounded p-1 cursor-pointer hover:bg-zinc-100"
+                    >
+                      {type.name}
+                    </div>
+                  ))}
+                </div>
+              </div>
+              <div>
+                <h4 className="text-xs font-bold">Superficie</h4>
+                <p className="text-xs text-muted-foreground mt-1">
+                  Selecciona un rango de superficie
+                </p>
+                <div className="grid grid-cols-2 gap-2 mt-2">
+                  <div className="text-xs border rounded p-1 cursor-pointer hover:bg-zinc-100">
+                    <span>0 - 500 m²</span>
+                  </div>
+                  <div className="text-xs border rounded p-1 cursor-pointer hover:bg-zinc-100">
+                    <span>500 - 1000 m²</span>
+                  </div>
+                  <div className="text-xs border rounded p-1 cursor-pointer hover:bg-zinc-100">
+                    <span>1000 - 2000 m²</span>
+                  </div>
+                  <div className="text-xs border rounded p-1 cursor-pointer hover:bg-zinc-100">
+                    <span>2000 - 5000 m²</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
       <div className="min-h-screen col-start-3 col-span-4 border-l">
-        <div className="absolute m-2 top-0 right-0 text-xs text-muted-foreground rounded border p-1 bg-muted opacity-50 hover:opacity-100">
-          Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
-        </div>
-        <div ref={mapContainer} className="min-h-screen"></div>
+        <Map />
       </div>
     </main>
   );
---
+++ components/map.tsx
@@ -0,0 +1,100 @@
+"use client";
+
+import mapboxgl from "mapbox-gl";
+import "mapbox-gl/dist/mapbox-gl.css";
+import { useRef, useEffect, useState } from "react";
+
+mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAPBOX_TOKEN || "";
+
+export default function Map() {
+  const mapContainer = useRef<HTMLDivElement>();
+  const map = useRef<mapboxgl.Map | null>(null);
+  const [lng, setLng] = useState(-111.3415);
+  const [lat, setLat] = useState(26.0143);
+  const [zoom, setZoom] = useState(14.13);
+
+  useEffect(() => {
+    // Initialize map only once
+    if (map.current) return;
+
+    map.current = new mapboxgl.Map({
+      container: mapContainer.current,
+      style: "mapbox://styles/kenneth-quintero/cm043gkrp00eo01pwcjwxfbmj",
+      center: [lng, lat],
+      zoom: zoom,
+    });
+
+    // Add navigation controls
+    map.current.addControl(new mapboxgl.NavigationControl());
+
+    map.current.on("move", () => {
+      setLng(map.current.getCenter().lng.toFixed(4));
+      setLat(map.current.getCenter().lat.toFixed(4));
+      setZoom(map.current.getZoom().toFixed(2));
+    });
+
+    map.current.on("load", () => {
+      map.current.on("click", "ezone-uso-de-suelo", (e) => {
+        const properties = e.features[0].properties;
+        setSelectedFeatureProperties(properties);
+        console.log(properties);
+      });
+
+      // Add a source for the highlighted polygon
+      map.current.addSource("highlighted-polygon", {
+        type: "geojson",
+        data: {
+          type: "FeatureCollection",
+          features: [],
+        },
+      });
+
+      // Add a layer to display the highlighted polygon
+      map.current.addLayer({
+        id: "highlighted-polygon-layer",
+        type: "line",
+        source: "highlighted-polygon",
+        paint: {
+          "line-color": "#FF0000", // Outline color (e.g., red)
+          "line-width": 3, // Width of the outline
+        },
+      });
+
+      // Detect clicks on the polygon layer
+      map.current.on("click", "ezone-uso-de-suelo", function (e) {
+        var clickedPolygon = e.features[0];
+
+        // Update the source with the clicked polygon data
+        map.current.getSource("highlighted-polygon").setData({
+          type: "FeatureCollection",
+          features: [clickedPolygon],
+        });
+
+        // // Optionally, show the properties in a popup
+        // new mapboxgl.Popup()
+        //   .setLngLat(e.lngLat)
+        //   .setHTML(
+        //     `<strong>${clickedPolygon.properties.name}</strong><br>Type: ${clickedPolygon.properties.type}`
+        //   )
+        //   .addTo(map.current);
+      });
+
+      map.current.on("mouseenter", "ezone-uso-de-suelo", function () {
+        map.current.getCanvas().style.cursor = "pointer";
+      });
+
+      map.current.on("mouseleave", "ezone-uso-de-suelo", function () {
+        map.current.getCanvas().style.cursor = "";
+      });
+    });
+  });
+
+  return (
+    <div className="min-h-screen col-start-3 col-span-4 border-l relative">
+      <div className="absolute z-10 m-2 top-0 left-0 text-xs text-muted-foreground rounded border p-1 bg-muted opacity-50 hover:opacity-100">
+        Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
+      </div>
+      <div ref={mapContainer} className="min-h-screen"></div>
+    </div>
+  );
+}
